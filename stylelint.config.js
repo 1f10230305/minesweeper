c625035793ae2762ac192f4da6872b4e33178676
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-05-06 17:54:14 +0900

    chore: remove `prettier`, `eslint`

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: [
     'stylelint-config-standard',
     'stylelint-config-recess-order',
-    'stylelint-config-prettier',
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
